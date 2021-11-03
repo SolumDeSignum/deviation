db34ec9e17c3409690342f26e4e75773e89e890e
Faks <sia_dev@inbox.lv>
2021-11-03 16:49:40 +0200

    Services - DeviationPDFLoggerService: 1.) added: bugfix to avoid issues

--- src/Services/DeviationPDFLoggerService.php
+++ src/Services/DeviationPDFLoggerService.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace SolumDeSignum\Deviation\Services;
 
-use Barryvdh\DomPDF\Facade as PDF;
 use ErrorException;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Support\Facades\View;
 
+use function app;
 use function storage_path;
 
 class DeviationPDFLoggerService
 {
+    /**
+     * @throws BindingResolutionException
+     */
     public function store(MessageLogged $messageLogged): void
     {
         /**
@@ -20,13 +24,15 @@ class DeviationPDFLoggerService
          */
         $exception = $messageLogged->context['exception'];
 
-        PDF::loadView(
-            View::make('solumdesignum/deviation::mail.exceptions', [
-                'exception' => $exception->__toString()
-            ])
-                ->render()
-        )
+        app()
+            ->make('dompdf.wrapper')
+            ->loadHtml(
+                View::make('solumdesignum/deviation::mail.exceptions', [
+                    'exception' => $exception->__toString()
+                ])
+                    ->render()
+            )
             ->setPaper('B4', 'landscape')
-            ->save(storage_path("deviation/message-logged-$messageLogged->nameLog.pdf"));
+            ->save(storage_path("deviation/pdf/message-logged-$messageLogged->nameLog.pdf"));
     }
 }
